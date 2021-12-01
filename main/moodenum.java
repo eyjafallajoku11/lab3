5fa0e0869e2736b7757c9db3828c51d8772d6236
eyjafallajoku11 <47786206+eyjafallajoku11@users.noreply.github.com>
2021-12-01 10:42:03 +0300

    Delete main directory

--- main/moodenum.java
+++
@@ -1,5 +0,0 @@
-package main;
-
-public enum moodenum {
-    DENY, HAPPY, STRICT
-}
--- main/owl_interface.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-public interface owl_interface{
-    void cough(characters target);
-
-    void notice_sign(Sign sign);
-
-}
